c679f47ec0830d28252cf1e8e7d884e52e14a92f
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-11-13 21:06:02 +0900

    First commit

---
+++ qiita-app/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by Seigetsu on 2023/11/13
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
