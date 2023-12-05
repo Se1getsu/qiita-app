40665ca89c7a1481de3c2ffb35a42b04ef2cc7bc
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2023-12-05 11:39:14 +0900

    refactor: URLではなくURLRequestsで通信するようにした

--- qiita-app/ViewController.swift
+++ qiita-app/ViewController.swift
@@ -31,7 +31,10 @@ class QiitaRepository {
     /// - parameter title: タイトルの検索キーワード。
     func fetchArticles(count: Int, title: String = "") async throws -> [Article] {
         let url = URL(string: "https://qiita.com/api/v2/items?page=1&per_page=\(count)&query=title:\(title)")!
-        let (data, _) = try await URLSession.shared.data(from: url)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Accept")
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
         return try JSONDecoder().decode([Article].self, from: data)
     }
 }
